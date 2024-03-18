d14209b03de0b7257fb4e7443228d18c08b6b9b1
kubraaygun <kubraaygun@outlook.com.tr>
2024-03-18 16:20:50 +0300

    guncelleme yapildi

--- src/constant.js
+++ src/constant.js
@@ -1,15 +1,17 @@
 export const options = {
-  method: "GET",
-  url: "https://flight-radar1.p.rapidapi.com/flights/list-in-boundary",
+  method: 'GET',
+  url: 'https://flight-radar1.p.rapidapi.com/flights/list-in-boundary',
   params: {
-    bl_lat: "34503215",
-    bl_lng: "25324261",
-    tr_lat: "42869652",
-    tr_lng: "44552871",
-    limit: "300",
+    bl_lat: '34.503215',
+    bl_lng: '25.324261',
+    tr_lat: '42.869652',
+    tr_lng: '44.552871',
+    limit: '300'
   },
   headers: {
-    "X-RapidAPI-Key": "937115946amsh313633b3451c2aep10b6c1jsnbbaf8d60e3c7",
-    "X-RapidAPI-Host": "flight-radar1.p.rapidapi.com",
-  },
+    'X-RapidAPI-Key': 'b3cfd04374msh4eb45ee4f1d0c8dp1ead6fjsnfe67f330b395',
+    'X-RapidAPI-Host': 'flight-radar1.p.rapidapi.com'
+  }
 };
+
+	
